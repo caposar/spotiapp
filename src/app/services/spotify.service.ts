4757e1eae3147ceb7a6a5e452dec29f0bd6af0e6
Cesar Paredes <chpvhr@hotmail.com>
2021-10-02 14:42:44 -0300

    Refactorizando los métodos del servicio para no duplicar código
    13. Centralizar las peticiones hacia Spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,12 +10,18 @@ export class SpotifyService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getNewReleases() {
+  getQuery(query: string) {
     const headers = new HttpHeaders ({
       'Authorization': 'Bearer BQA9sUB5Bv5Jtx-gGj7rChY8eNVUxihv-e32OdY0rsdMA85gV8W7pvD-Xr_ZnuLQkebAQDYQ6hBwkuYnbPQ'
     });
 
-    return this.httpClient.get('https://api.spotify.com/v1/browse/new-releases?limit=5', {headers})
+    const url = `https://api.spotify.com/v1/${query}`;
+
+    return this.httpClient.get(url, {headers});
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases?limit=5')
     .pipe(
       map(data => {
         return data['albums'].items;
@@ -24,11 +30,7 @@ export class SpotifyService {
   }
 
   getArtista(termino: string): Observable<Object> {
-    const headers = new HttpHeaders ({
-      'Authorization': 'Bearer BQA9sUB5Bv5Jtx-gGj7rChY8eNVUxihv-e32OdY0rsdMA85gV8W7pvD-Xr_ZnuLQkebAQDYQ6hBwkuYnbPQ'
-    });
-
-    return this.httpClient.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers})
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
     .pipe(
       map(data => {
         return data['artists'].items;
