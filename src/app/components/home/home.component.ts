377dae3e3d374e1e9b027e358c0af6336e933f23
Cesar Paredes <chpvhr@hotmail.com>
2021-10-09 22:27:31 -0300

    Agregando notificación si se produce un error en el home
    21. Manejo de errores de un observable

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,15 +10,24 @@ export class HomeComponent implements OnInit {
 
   nuevasCanciones: any[] = [];
   loading: boolean;
+  error: boolean;
+  mensajeError: string;
 
   constructor(private spotifyService: SpotifyService) {
     this.loading = true;
+    this.error = false;
+    this.mensajeError = '';
   }
 
   ngOnInit() {
     this.spotifyService.getNewReleases().subscribe( (data: any) => {
       this.nuevasCanciones = data;
       this.loading = false;
+    }, (errorServicio) => {
+      this.loading = false;
+      this.error = true;
+      this.mensajeError = errorServicio.error.error.message;
+      console.log('Error Servicio: ', errorServicio);
     });
   }
 
